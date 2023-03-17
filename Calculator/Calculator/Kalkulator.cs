38296cb64d4244b92d6bbd177bf0292d7e3e000b
242447 Kwiatkowski Piotr <242447@edu.p.lodz.pl>
2023-03-17 18:16:31 +0100

    Dodanie nowych operacji w postaci metod do klasy Calculator.

--- Calculator/Calculator/Kalkulator.cs
+++ Calculator/Calculator/Kalkulator.cs
@@ -12,5 +12,22 @@
             return x - y;
         }
 
+        public int Mul(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Div(int x, int y)
+        {
+            if (x == 0 && y == 0)
+            {
+                throw new NotImplementedException("Operacja dzielenia przez 0 nie jest zdefiniowana.");
+            }
+            else
+            {
+                return x / y;
+            }
+        }
+
     }
 }
--- Calculator/CalculatorTest/CalculatorTests.cs
+++ Calculator/CalculatorTest/CalculatorTests.cs
@@ -27,5 +27,29 @@ namespace CalculatorTest
             Assert.AreEqual(kalkulator.Sub(-3, -1), -2);
             Assert.AreEqual(kalkulator.Sub(4, 0), 4);
         }
+
+        [TestMethod]
+        public void TestMul()
+        {
+            Kalkulator kalkulator = new Kalkulator();
+            Assert.AreEqual(kalkulator.Mul(0, 10), 0);
+            Assert.AreEqual(kalkulator.Mul(5, 0), 0);
+            Assert.AreEqual(kalkulator.Mul(5, 3), 15);
+            Assert.AreEqual(kalkulator.Mul(-2, 3), -6);
+            Assert.AreEqual(kalkulator.Mul(5, -7), -35);
+            Assert.AreEqual(kalkulator.Mul(-5, -1), 5);
+        }
+
+        [TestMethod]
+        public void TestDiv()
+        {
+            Kalkulator kalkulator = new Kalkulator();
+            Assert.AreEqual(kalkulator.Div(0, 3), 0);
+            Assert.AreEqual(kalkulator.Div(5, 10), 0,5);
+            Assert.AreEqual(kalkulator.Div(-20, 10), -2);
+            Assert.AreEqual(kalkulator.Div(42, -6), -7);
+            Assert.AreEqual(kalkulator.Div(-33, -6), 5,5);
+            Assert.ThrowsException<NotImplementedException>(() => kalkulator.Div(0, 0));
+        }
     }
 }
