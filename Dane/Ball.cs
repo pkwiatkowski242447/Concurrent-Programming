34621aecda9962bbf89441dac1452e9f7c83a000
Aleksander Janicki <janicki.olek77@gmail.com>
2023-04-14 21:24:15 +0200

    Poprawa struktury katalogów

---
+++ Dane/Ball.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dane
+{
+    public class Ball
+    {
+        private double massOfTheBall;
+        private Position centerOfTheBall;
+        private int ballRadius;
+        private Position velocityVector;
+
+        public Ball(double massOfTheBall, Position centerOfTheBall, int ballRadius, Position velocityVector)
+        {
+            this.massOfTheBall = massOfTheBall;
+            this.centerOfTheBall = centerOfTheBall;
+            this.ballRadius = ballRadius;
+            this.velocityVector = velocityVector;
+        }
+
+        /*
+         * Getters
+         */
+
+        public double getBallMass()
+        {
+            return this.massOfTheBall;
+        }
+
+        public Position getCenterOfTheBall()
+        {
+            return this.centerOfTheBall;
+        }
+
+        public int getBallRadius()
+        {
+            return this.ballRadius;
+        }
+
+        public Position getVelocityVector()
+        {
+            return this.velocityVector;
+        }
+
+        /*
+         * Setter methods.
+         */
+
+        public void setCenterOfTheBall(Position newCenter)
+        {
+            this.centerOfTheBall = newCenter;
+        }
+
+        public void setVelocityVector(Position newVelocity)
+        {
+            this.velocityVector = newVelocity;
+        }
+
+
+    }
+}
---
+++ Dane/Position.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dane
+{
+    public class Position
+    {
+        public int xCoordinate;
+        public int yCoordinate;
+
+        public Position(int xCoordinate, int yCoordinate)
+        {
+            this.xCoordinate = xCoordinate;
+            this.yCoordinate = yCoordinate;
+        }
+
+        public int getXCoordinate()
+        {
+            return this.xCoordinate;
+        }
+
+        public int getYCoordinate()
+        {
+            return this.yCoordinate;
+        }
+    }
+}
