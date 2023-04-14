cc9d063042e6af4d69b50076c76eb7a0f5e6de1a
242447 Kwiatkowski Piotr <242447@edu.p.lodz.pl>
2023-04-14 16:52:36 +0200

    "Wstępna struktura katalogów do Etapu #1"

---
+++ Dane/Position.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dane
+{
+    public class Position
+    {
+        public int xCoordinate;
+        public int yCoordinate;
+
+        public Position(int xCoordinate, int yCoordinate)
+        {
+            this.xCoordinate = xCoordinate;
+            this.yCoordinate = yCoordinate;
+        }
+
+        public int getXCoordinate()
+        {
+            return this.xCoordinate;
+        }
+
+        public int getYCoordinate()
+        {
+            return this.yCoordinate;
+        }
+    }
+}
