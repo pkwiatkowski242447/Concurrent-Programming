a84a11b03f29823b61319d5b095846916886be33
eljotero <janicki.olek77@gmail.com>
2023-05-14 20:59:22 +0200

    Mergowanie Etap 2 1/2 cd

--- LogikaTest/LogicTests.cs
+++ LogikaTest/LogicTests.cs
@@ -50,7 +50,7 @@ namespace LogicTest
 
         internal class Ball : DataBallInterface
         {
-            public override double MassOfTheBall { get; set; }
+            public override double MassOfTheBall { get; }
             public override DataPositionInterface CenterOfTheBall { get; }
             public override DataPositionInterface VelocityVectorOfTheBall { get; set; }
             public override bool StopTask { get; set; }
